f3cd2aceaf555930c22aacc682ff182fcaa90d14
FacuM87 <facundo.mingorance@hotmail.com>
2024-11-06 18:58:05 -0300

    clean up en messagesContainer

--- client/src/components/MessagesContainer.jsx
+++ client/src/components/MessagesContainer.jsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react"
 import { useConversation } from "../hooks/userStore"
 import Message from "./Message"
 
 
 const MessagesContainer = () => {
-    const {selectedConversation} = useConversation()
+    const {selectedConversation, setSelectedConversation} = useConversation()
+    
+    useEffect(() => {
+        return () => setSelectedConversation(null)
+     },[setSelectedConversation])
 
     return (
     <div className="flex flex-col overflow-auto p-4 flex-grow">
