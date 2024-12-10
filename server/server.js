3450c9916fcce3c268ed2549aa6f91b7cb517a38
FacuM87 <facundo.mingorance@hotmail.com>
2024-12-10 02:46:51 -0300

    set up para deploy

--- server/server.js
+++ server/server.js
@@ -16,10 +16,13 @@ dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-})) // SOLO PARA DESARROLLO
+
+if(process.env.NODE_ENV === "development") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    }))
+}
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
