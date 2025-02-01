4dcaf7ccd066584a5f674b9d95ff16f6f9b2632e
FacuM87 <facundo.mingorance@hotmail.com>
2025-02-01 03:15:20 -0300

    mejora responsive

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -4,7 +4,7 @@ import ChatBoxInput from "./ChatBoxInput";
 
 const ChatBox = () => {
   return (
-    <div className="md:min-w-[450px] flex flex-col border-l border-gray-300">
+    <div className="md:min-w-[450px] sm:min-w-[450px] flex flex-col border-l border-gray-300">
       <ChatBoxHeader />
       <MessagesContainer />
       <ChatBoxInput />
